16edb223f5084a5110c2d30b34da0b153c439811
Gabriel Paiva <gabrielmpaiva0703@gmail.com>
2026-04-08 11:55:14 -0300

    Alteração na query

--- app/api/deadlines/route.js
+++ app/api/deadlines/route.js
@@ -2,7 +2,7 @@ import { totvsQuery } from '../../lib/totvs'
 
 export async function GET() {
   const result = await totvsQuery(
-    "SELECT E4_CODIGO,E4_COND,E4_DESCRI,E4_XFATOR FROM SE4010 WHERE D_E_L_E_T_ = ' '"
+    "SELECT E4_CODIGO,E4_COND,E4_DESCRI,E4_XFATOR FROM SE4010 WHERE D_E_L_E_T_ = ' ' AND E4_XFATOR > 0"
   )
   return Response.json(result)
 }
