329edad683cdbb7356c4f2e05f5b18b494b75642
Gabriel Paiva <gabrielmpaiva0703@gmail.com>
2026-04-08 15:45:43 -0300

    ajuste deploy5

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverExternalPackages: ['ssh2-sftp-client', 'ssh2'],
-  },
-  bundler: 'webpack',
+  serverExternalPackages: ['ssh2-sftp-client', 'ssh2'],
 }
 
 module.exports = nextConfig
